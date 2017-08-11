3f7b46f8e7834d28b158ffb00f938067cf0601c5
sontran2016 <son.tran@newoceaninfosys.com>
2017-08-11 20:49:33 +0700

    aa
    ss

---
+++ WApplication1/WApplication1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WApplication1
+{
+    class Class1
+    {
+        public int func1()
+        {
+            int a, b;
+            a = 1;
+            b = 2;
+            int c = a + b;
+            return c;
+        }
+    }
+}
